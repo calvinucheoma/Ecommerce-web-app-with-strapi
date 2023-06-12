6146124fc03236bed9bc2d01c306f5092ffb7b68
Calvin Ucheoma <calvinchukwuma@gmail.com>
2023-06-12 17:47:40 +0100

    strapi backend

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,21 +1,20 @@
-module.exports = [
-  "strapi::errors",
-  "strapi::security",
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
-];
-
-module.exports = {
+module.exports = ({ env }) => ({
+  load: [
+    "strapi::errors",
+    "strapi::security",
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::logger",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
+  ],
   settings: {
     cors: {
       enabled: true,
       origin: "*",
     },
   },
-};
+});
